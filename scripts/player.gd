5142aa29e0b0ba567387e2d1e4ab534d38fd9f04
Leirda01 <adrieldj@orange.fr>
2020-07-10 23:03:36 +0200

    Create basic grid based movement.
    Made following https://kidscancode.org/godot_recipes/2d/grid_movement/

---
+++ scripts/player.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+onready var ray = $RayCast2D
+
+var tile_size = 64
+var inputs = {"right": Vector2.RIGHT,
+				"left": Vector2.LEFT,
+				"up": Vector2.UP,
+				"down": Vector2.DOWN}
+
+func _ready():
+	position = position.snapped(Vector2.ONE * tile_size)
+	position += Vector2.ONE * tile_size / 2
+
+func _unhandled_input(event):
+	for direction in inputs.keys():
+		if event.is_action_pressed(direction):
+			move(direction)
+
+func move(dir):
+	ray.cast_to = inputs[dir] * tile_size
+	ray.force_raycast_update()
+	if not ray.is_colliding():
+		position += inputs[dir] * tile_size
