12efc5b273bc695f20d87afb3419fa707e94fa41
Leirda01 <adrieldj@orange.fr>
2020-07-11 13:45:20 +0200

    Explain what is needed to be done

--- scripts/Cursor.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-func play_turn(object: Object):
-	visible = true
-	position = object.global_position
-	yield(get_tree().create_timer(1.0), "timeout")
-	object.move(Vector2.LEFT)
-	visible = false
--- scripts/Main.gd
+++ scripts/Main.gd
@@ -9,9 +9,17 @@ const directions = {
 	"ui_down": Vector2.DOWN,
 }
 
-func _ready():
-	play()
+func _process(_delta):
+	pass
+	# si tout les ennemis n’ont pas bougé:
+	# 	return
+	# bouge le joueur
+	# dis au programme de s’occuper des attaques
 
-func play():
-	for enemy in $Enemies.get_children():
-		yield($Cursor.play_turn(enemy), "completed")
+func _input(event):
+	pass
+	# si il reste des ennemis qui n’ont pas bougé
+	# 	si une touche de la liste est pressée
+	# 		si ce n’est pas une touche valide
+	# 			return
+	# 		l’ennemi.move(la direction de la touche)
