a7c42d1fc672b17973868bf8df23c62e5bd978c0
Leirda01 <adrieldj@orange.fr>
2020-07-11 03:05:13 +0200

    Add basic enemy

---
+++ scripts/Enemy.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+
+func move(direction: Vector2):
+	$Entity.move(direction)
--- scripts/Main.gd
+++ scripts/Main.gd
@@ -1,6 +1,15 @@
 extends Node
 
+const directions = {
+	"ui_right": Vector2.RIGHT,
+	"ui_up": Vector2.UP,
+	"ui_left": Vector2.LEFT,
+	"ui_down": Vector2.DOWN,
+}
 
 func _unhandled_input(event):
 	if event.is_action_pressed("ui_select"):
 		$Player.random_move()
+	for dir in directions.keys():
+		if event.is_action_pressed(dir):
+			$Enemy.move(directions[dir])
