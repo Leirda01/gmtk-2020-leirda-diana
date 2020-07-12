96fa719618cac7275825f2fb6c7a2507451cb771
Leirda01 <adrieldj@orange.fr>
2020-07-12 13:04:25 +0200

    Add moving balloon for player

--- scripts/Spawner.gd
+++ scripts/Spawner.gd
@@ -1,13 +1,6 @@
 extends Area2D
 
-var can_spawn: = true
 
 func add_enemy(parent: Object, object: Object):
-	if can_spawn:
-		object.get_node("Controller").position = self.position
-		parent.add_child(object)
-		can_spawn = false
-
-
-func _on_Spawner_body_exited(body):
-	can_spawn = true
+	object.get_node("Controller").position = self.position
+	parent.add_child(object)
